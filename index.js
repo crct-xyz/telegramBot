9f4bae7df100f5aa5384acef6615986a7f503a25
TristHund <TristanHundley@protonmail.com>
2024-10-07 17:25:56 +0200

    merging in deployed code

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
 const TelegramBot = require("node-telegram-bot-api");
-<<<<<<< HEAD
-const { Connection, PublicKey, clusterApiUrl } = require("@solana/web3.js");
-
-// Initialize Telegram bot with webhook
-const bot = new TelegramBot("7216921050:AAEISmruLCEXGap4zLcpDyzGyLKWTIBq2SU", {
-  webHook: true,
-});
-bot.setWebHook(
-  "https://bketuxt5md.execute-api.eu-central-1.amazonaws.com/TelegramBot"
-);
-
-// Initialize Solana connection
-const connection = new Connection(clusterApiUrl("mainnet-beta"));
-let multisigPda;
-const subscribers = new Set();
-=======
 const AWS = require("aws-sdk");
 const axios = require("axios");
 
@@ -35,39 +19,6 @@ const actionsDbURL = "http://ec2-52-59-228-70.eu-central-1.compute.amazonaws.com
 
 let username;
 let recipients;
-// get blink url and telegram username from the notification sqs
-// async function receiveSQSMessages() {
-//   const params = {
-//     QueueUrl: notificationQueueUrl,
-//     MaxNumberOfMessages: 10,
-//     WaitTimeSeconds: 5,
-//   };
-
-//   try {
-//     const result = await sqs.receiveMessage(params).promise();
-
-//     if (result.Messages && result.Messages.length > 0) {
-//       for (const message of result.Messages){
-//         const receivedMessage = message;
-//         console.log("Message received:", receivedMessage);
-
-//         // Process the message
-//         console.log("Message Body:", receivedMessage.Body);
-//         const bodyJSON = JSON.parse(receivedMessage.Body)
-//         console.log("receiptHandle: ", message.ReceiptHandle)
-//         username = bodyJSON.sendData.telegram_user;
-//         blink_url = bodyJSON.sendData.blinkUrl
-//         console.log("username: ", username)
-//         console.log("blink URL: ", blink_url);
-//         await deleteMessage(message.ReceiptHandle);
-//       };
-//     } else {
-//       console.log("No messages to process.");
-//     }
-//   } catch (error) {
-//     console.log("error: ", error);
-//   }
-// }
 
 async function deleteMessage(receiptHandle) {
   const deleteParams = {
@@ -83,102 +34,10 @@ async function deleteMessage(receiptHandle) {
   }
 }
 
->>>>>>> bot/full-functions
 
 // Main Lambda handler
 exports.handler = async (event) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
-<<<<<<< HEAD
-
-  if (!event.body) {
-    console.log("body not provided");
-  }
-  
-  const update = JSON.parse(event.body);
-  await bot.processUpdate(update);
-
-  async function checkTransactions() {
-    if (!multisigPda) {
-      console.error("Multisig PDA not set. Please use /setMultisig to set it.");
-      return;
-    }
-    let lastCheckedSignature;
-    const signatures = await connection.getSignaturesForAddress(multisigPda, {
-      limit: 5,
-    });
-    console.log(signatures);
-    for (const sigInfo of signatures) {
-      if (sigInfo.signature === lastCheckedSignature) {
-        break;
-      }
-      if (sigInfo.confirmationStatus === "finalized") {
-        const message = `new transaction detected: \nSignature: ${sigInfo.signature}\nSlot: ${sigInfo.slot}\nVote for the transaction here: https://dial.to/?action=solana-action%3Ahttp://localhost:3000/api/actions/squad/vote?address=${multisigPda}`;
-        lastCheckedSignature = sigInfo.signature;
-        sendTelegramMessage(message);
-        break;
-      }
-    }
-  }
-  function sendTelegramMessage(message) {
-    if (subscribers.length === 0) {
-        console.log("subscribers array empty");
-        return;
-    }
-    subscribers.forEach((chatId) => {
-      bot.sendMessage(chatId, message);
-    });
-  }
-
-  if (event.source && event["detail-type"]) {
-    // Event is coming from EventBridge
-    console.log("Event is from EventBridge");
-    await checkTransactions();
-  }
-
-
-  // Handle specific commands (e.g., /start) here
-  if (update.message && update.message.text === "/start") {
-    const chatId = update.message.chat.id;
-    subscribers.add(chatId);
-    console.log(subscribers);
-    await bot.sendMessage(chatId, "Bot has started");
-    return;
-  }
-  if (update.message && update.message.text.startsWith("/setMultisig ")) {
-    const chatId = update.message.chat.id;
-
-    // Extract the multisig address from the message
-    const parts = update.message.text.split(" ");
-    if (parts.length === 2) {
-      const multisigAddress = parts[1];
-
-      // Validate the address (basic length check for a Solana address)
-      if (multisigAddress.length === 44) {
-        // Solana addresses are typically 44 characters long
-        multisigPda = new PublicKey(multisigAddress);
-        await bot.sendMessage(
-          chatId,
-          `Multisig address set successfully: ${multisigPda.toBase58()}`
-        );
-      } else {
-        await bot.sendMessage(
-          chatId,
-          "Invalid address. Please provide a valid Solana multisig address."
-        );
-      }
-    } else {
-      await bot.sendMessage(chatId, "Usage: /setMultisig <address>");
-    }
-  }
-
-  await checkTransactions();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Webhook received" }),
-  };
-};
-=======
   // await receiveSQSMessages();
 
   for (const record of event.Records) {
@@ -193,8 +52,6 @@ exports.handler = async (event) => {
   const response = await axios.get(dbUrl);
   const data = response.data;
 
-  // get the action based on the action id
-  // const res = await axios.get(actionsDbURL);
   const actionID = 7
   const blink_url = `https://dial.to/?action=solana-action%3Ahttps://squint-api.vercel.app/actions/${actionID}`
 
@@ -316,4 +173,3 @@ bot.onText(/\/help/, (msg) => {
     "Available commands:\n/start - Start the bot\n/help - Show this help message",
   );
 });
->>>>>>> bot/full-functions
